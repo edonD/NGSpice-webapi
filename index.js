7a9036ef64b70e45ac72f62a1c9148e2cc1be1a7
edonD <edoonn.d@gmail.com>
2023-04-30 19:03:32 +0200

    add create file

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { runNgspice } from "./runNgspice.js";
+import fs from "fs";
 
 const app = express();
 
+app.use(express.json()); // Enable JSON request bodies
+
+// Run ngspice with a file
 app.get("/run-ngspice", async (req, res) => {
   const file = req.query.file;
   if (!file) {
@@ -16,6 +20,26 @@ app.get("/run-ngspice", async (req, res) => {
   } catch (error) {
     res.status(500).send(error.message);
   }
+});
+
+// Create a .cir file
+app.post("/create-cir-file", (req, res) => {
+  const content = req.body.content;
+  if (!content) {
+    res.status(400).send("Missing content parameter");
+    return;
+  }
+
+  // Write the content to a file
+  const filename = "new-file.cir";
+  fs.writeFile(filename, content, (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error writing file");
+    } else {
+      res.send(`File '${filename}' created successfully`);
+    }
+  });
 });
 
 const port = process.env.PORT || 3000;
