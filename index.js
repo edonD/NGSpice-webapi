9d0a0bad329df02f5b1de45eaca14474e4057da3
edonD <edoonn.d@gmail.com>
2023-05-01 02:05:16 +0200

    reduce the complexity

--- index.js
+++ index.js
@@ -1,66 +1,41 @@
 import express from "express";
-import { runNgspice } from "./runNgspice.js";
-import { parseNgspiceOutput } from "./parseNgspiceOutput.js";
 import fs from "fs";
-import { Chart } from "chart.js";
-
-import { createCanvas } from "canvas";
+import { spawn } from "child_process";
 
 const app = express();
 
-app.use(express.json()); // Enable JSON request bodies
+// Define a route to run ngspice and save the output data locally
+app.get("/run-ngspice", (req, res) => {
+  // Define the ngspice command to execute
+  const ngspice = spawn("ngspice", ["-b", "-o", "output.txt", "circuit.cir"]);
 
-// Run ngspice with a file and plot the results
-app.get("/run-ngspice", async (req, res) => {
-  const file = req.query.file;
-  if (!file) {
-    res.status(400).send("Missing file parameter");
-    return;
-  }
+  // Handle stdout and stderr data
+  ngspice.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
 
-  try {
-    const stdout = await runNgspice(file);
-    const data = parseNgspiceOutput(stdout);
+  ngspice.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
 
-    // Write the simulation data to a file
-    const filename = "simulation-data.txt";
-    fs.writeFile(filename, stdout, (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error writing file");
-      } else {
-        // Send the file as a response with the appropriate content type
-        res.set("Content-Type", "text/plain");
-        res.attachment(filename);
-        res.sendFile(filename, { root: __dirname });
-      }
-    });
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-});
+  // Handle ngspice process exit event
+  ngspice.on("exit", (code) => {
+    console.log(`Ngspice process exited with code ${code}`);
 
-// Create a .cir file
-app.post("/create-cir-file", (req, res) => {
-  const content = req.body.content;
-  if (!content) {
-    res.status(400).send("Missing content parameter");
-    return;
-  }
+    // Check if output file exists
+    if (fs.existsSync("output.txt")) {
+      // Read the output data from the file
+      const outputData = fs.readFileSync("output.txt", "utf8");
 
-  // Write the content to a file
-  const filename = "new-file.cir";
-  fs.writeFile(filename, content, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error writing file");
+      // Send the output data as a response
+      res.send(outputData);
     } else {
-      res.send(`File '${filename}' created successfully`);
+      res.status(500).send("Output file not found");
     }
   });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Start the server
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
